3e8e2755d0de57aee6276f16d2dcefa8b1aaa0d5
Klemen Sever <achedeuzot@users.noreply.github.com>
2018-09-27 18:49:34 +0200

    fix(test): Fixed typo in test (#7)

--- test/test.js
+++ test/test.js
@@ -29,23 +29,23 @@ describe('sanitizeUrl', function () {
   });
 
   it('replaces data urls with about:blank', function () {
-    expect(sanitizeUrl('data:text/html;basfe64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
+    expect(sanitizeUrl('data:text/html;base64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
   });
 
   it('replaces data urls with about:blank when data url begins with %20', function () {
-    expect(sanitizeUrl('%20%20%20%20data:text/html;basfe64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
+    expect(sanitizeUrl('%20%20%20%20data:text/html;base64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
   });
 
   it('replaces data urls with about:blank when data url begins with \s', function () {
-    expect(sanitizeUrl('    data:text/html;basfe64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
+    expect(sanitizeUrl('    data:text/html;base64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
   });
 
   it('disregards capitalization for data urls', function () {
-    expect(sanitizeUrl('dAtA:text/html;basfe64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
+    expect(sanitizeUrl('dAtA:text/html;base64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E')).to.equal('about:blank');
   });
 
   it('ignores ctrl characters in data urls', function () {
-    expect(sanitizeUrl(decodeURIComponent('dat%0aa:text/html;basfe64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E'))).to.equal('about:blank');
+    expect(sanitizeUrl(decodeURIComponent('dat%0aa:text/html;base64,PH%3Cscript%3Ealert(document.domain)%3C/script%3E'))).to.equal('about:blank');
   });
 
   it('does not alter http URLs', function () {
